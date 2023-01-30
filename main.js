a2574f234f1ea91984a111f2f11ed38cba63c978
Joao Portugal <joaosportugal@hotmail.com>
2023-01-30 17:11:26 -0300

    add: here has been started the carosel function production

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
 const selectBox = boxNumber => document.getElementsByClassName(boxNumber)[0];
 
-// aparecer botao de visualizar com o mouseover
-selectBox('project_1').addEventListener('mouseover', function() {
-    const githubLink = selectBox('project_1').getElementsByClassName('githubLink')[0];
-    const deployLink = selectBox('project_1').getElementsByClassName('deployLink')[0];
-    const boxContent = selectBox('project_1').getElementsByClassName('boxContent')[0];
+// criando manipulações no project preview
+function eachProjectPreview(projectNumber, projectDescriptionNumber) {
+    const githubLink = selectBox(projectNumber).getElementsByClassName('githubLink')[0];
+    const deployLink = selectBox(projectNumber).getElementsByClassName('deployLink')[0];
+    const boxContent = selectBox(projectNumber).getElementsByClassName('boxContent')[0];
     const inicialText = document.getElementById('inicialText');
-    boxContent.classList.add('hoverEffect');
-    githubLink.style.display = 'block';
-    deployLink.style.display = 'block';
-    inicialText.style.display = 'none';
-})
+    const eachProjectDescription = document.getElementsByClassName(projectDescriptionNumber)[0];
 
-// sumir botao de visualizar com o mouseout
-selectBox('project_1').addEventListener('mouseout', function() {
-    const githubLink = selectBox('project_1').getElementsByClassName('githubLink')[0];
-    const deployLink = selectBox('project_1').getElementsByClassName('deployLink')[0];
-    const boxContent = selectBox('project_1').getElementsByClassName('boxContent')[0];
-    const inicialText = document.getElementById('inicialText');
-    boxContent.classList.remove('hoverEffect');
-    githubLink.style.display = 'none';
-    deployLink.style.display = 'none';
-    inicialText.style.display = 'block';
-    
-})
+    //mouseover
+    selectBox(projectNumber).addEventListener('mouseover', function() {
+        boxContent.classList.add('hoverEffect');
+        githubLink.style.display = 'block';
+        deployLink.style.display = 'block';
+        inicialText.style.display = 'none';
+        eachProjectDescription.style.display = 'flex';
+    })
 
-// aparecer descrição do projeto com mouseover
-selectBox('project_1').addEventListener('mouseover', function () {
-    const project_1 = document.getElementsByClassName('project_description')[0];
-    project_1.style.display = 'flex'
-})
-
-// sumir descrição do projeto com mouseout
-selectBox('project_1').addEventListener('mouseout', function () {
-    const project_1 = document.getElementsByClassName('project_description')[0];
-    project_1.style.display = 'none'
-})
-
-function addBand(projectBox) {
-    const band = selectBox(projectBox).getElementsByClassName('boxContent')[0].firstChild;
-    const boxText = selectBox(projectBox).getElementsByClassName('boxText')[0];
-    boxText.innerHTML = 'EM BREVE';
-    band.classList.add('band');
+    //mouseout
+    selectBox(projectNumber).addEventListener('mouseout', function() {
+        boxContent.classList.remove('hoverEffect');
+        githubLink.style.display = 'none';
+        deployLink.style.display = 'none';
+        inicialText.style.display = 'block';
+        eachProjectDescription.style.display = 'none';
+    })
 }
 
+function addBand(projectNumber) {
+    const boxContent = selectBox(projectNumber).getElementsByClassName('boxContent')[0];
+    boxContent.innerHTML = '<div><span class="boxText">EM BREVE</span></div>'
+    boxContent.firstChild.classList.add('band');
+}
+
+eachProjectPreview('project_1', 'project_1_description');
+eachProjectPreview('project_2', 'project_2_description');
+eachProjectPreview('project_3', 'project_3_description');
+// eachProjectPreview('project_4', 'project_3_description');
+// eachProjectPreview('project_5', 'project_3_description');
+// eachProjectPreview('project_6', 'project_3_description');
+// eachProjectPreview('project_7', 'project_3_description');
+// eachProjectPreview('project_8', 'project_3_description');
+
 // addBand('project_1');
-addBand('project_2');
-addBand('project_3');
+// addBand('project_2');
+// addBand('project_3');
 addBand('project_4');
+addBand('project_5');
+addBand('project_6');
+addBand('project_7');
+addBand('project_8');
