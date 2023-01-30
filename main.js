557063d86798cacfe61a761b9db926011ecd3148
Joao Portugal <joaosportugal@hotmail.com>
2023-01-30 13:07:34 -0300

    including github and deploy links in the boxes

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 const selectBox = boxNumber => document.getElementsByClassName(boxNumber)[0];
 
 // aparecer botao de visualizar com o mouseover
-
 selectBox('project_1').addEventListener('mouseover', function() {
-    (function visualizeButton() {
-        
-    })()
+    const boxText = selectBox('project_1').getElementsByClassName('boxText')[0];
+    const boxContent = selectBox('project_1').getElementsByClassName('boxContent')[0];
+    const inicialText = document.getElementById('inicialText');
+    boxContent.classList.add('hoverEffect');
+    boxText.innerHTML = 'visualizar';
+    boxText.style.fontSize = '1.5em';
+    inicialText.style.display = 'none';
 })
 
+// sumir botao de visualizar com o mouseout
+selectBox('project_1').addEventListener('mouseout', function() {
+    const boxText = selectBox('project_1').getElementsByClassName('boxText')[0];
+    const boxContent = selectBox('project_1').getElementsByClassName('boxContent')[0];
+    const inicialText = document.getElementById('inicialText');
+    boxContent.classList.remove('hoverEffect');
+    boxText.innerHTML = ''
+    inicialText.style.display = 'block';
+})
+
+// aparecer descrição do projeto com mouseover
+selectBox('project_1').addEventListener('mouseover', function () {
+    const project_1 = document.getElementsByClassName('project_description')[0];
+    project_1.style.display = 'flex'
+})
+
+// sumir descrição do projeto com mouseout
+selectBox('project_1').addEventListener('mouseout', function () {
+    const project_1 = document.getElementsByClassName('project_description')[0];
+    project_1.style.display = 'none'
+})
 
 function addBand(projectBox) {
     const band = selectBox(projectBox).getElementsByClassName('boxContent')[0].firstChild;
